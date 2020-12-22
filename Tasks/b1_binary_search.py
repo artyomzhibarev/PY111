a8c355284e79918a0f6fae7b20280b99648d1179
Artyom Zhibarev <artyom.zhibarev@yandex.ru>
2020-12-22 21:24:13 +0300

    test_borders passed. test_existing not passed. test_missing passed.

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,5 +9,25 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+    mid = 0
+    start = 0
+    end = len(arr)
+    while start <= end:
+        if elem not in arr:
+            return None
+        mid = (start + end) // 2
+        print(mid)
+        print(end)
+        if elem == arr[mid]:
+            return mid
+        if elem < arr[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return mid
+
+
+if __name__ == '__main__':
+    test_ = 2, [1, 2, 2, 2]
+    list_ = [324, 311, 56, 7, 12, 67, 55, 7, 787]
+    print(binary_search(2, [1, 2, 2, 2]))
