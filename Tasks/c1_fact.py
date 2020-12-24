d6da89de6ed757659ec3277e4bdbcc133d96b04b
Artyom Zhibarev <artyom.zhibarev@yandex.ru>
2020-12-24 19:06:56 +0300

    Tests passed

--- Tasks/c1_fact.py
+++ Tasks/c1_fact.py
@@ -4,8 +4,11 @@ def factorial_recursive(n: int) -> int:
     :param n: int > 0
     :return: factorial of n
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError
+    if n == 0:
+        return 1
+    return n * factorial_recursive(n - 1)
 
 
 def factorial_iterative(n: int) -> int:
@@ -15,5 +18,9 @@ def factorial_iterative(n: int) -> int:
     :param n: int > 0
     :return: factorial of n
     """
-    print(n)
-    return 0
+    f = 1
+    if n < 0:
+        raise ValueError
+    for i in range(1, n + 1):
+        f *= i
+    return f
