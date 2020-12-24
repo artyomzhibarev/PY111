3e0acdf6013bf5822b2fb88b6186c4130df0c5f3
Artyom Zhibarev <artyom.zhibarev@yandex.ru>
2020-12-24 18:49:14 +0300

    fib_recursive, fib_iterative created. Tests passed

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -5,8 +5,14 @@ def fib_recursive(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n >= 2:
+        return fib_recursive(n - 1) + fib_recursive(n - 2)
 
 
 def fib_iterative(n: int) -> int:
@@ -16,5 +22,10 @@ def fib_iterative(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    fib1 = 0
+    fib2 = 1
+    if n < 0:
+        raise ValueError
+    for i in range(n):
+        fib1, fib2 = fib2, fib1 + fib2
+    return fib1
