0f7b076b1ff8b9e9548ac703612c9cba7ee1ed21
Artyom Zhibarev <artyom.zhibarev@yandex.ru>
2020-12-25 10:24:15 +0300

    binary_search_recursive function created. All tests passed

--- Tasks/c2_recursive_binary_search.py
+++ Tasks/c2_recursive_binary_search.py
@@ -9,5 +9,20 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+
+    def binary_search_recursive(left: int, right: int):
+        if left > right:
+            return None
+        mid = left + (right - left) // 2
+        if arr[mid] == elem:
+            while arr[mid - 1] == elem:
+                mid -= 1
+            return mid
+        elif arr[mid] < elem:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+        return binary_search_recursive(left, right)
+
+    return binary_search_recursive(0, len(arr) - 1)
