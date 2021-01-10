fa26f931c6a02654f78f803e8903a83a0b6da777
Artyom Zhibarev <artyom.zhibarev@yandex.ru>
2021-01-10 21:33:52 +0300

    test passed

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,4 +1,7 @@
+from collections import deque
 from typing import Hashable, List
+
+import matplotlib.pyplot as plt
 import networkx as nx
 
 
@@ -16,5 +19,46 @@ def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node for search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    # drow_graph(g)
+
+    visited = {node: False for node in g.nodes}  # словарь непосещенных вершин
+    wait_nodes = deque()  # подоженные
+
+    wait_nodes.append(start_node)
+    visited[start_node] = True
+    path = []
+
+    while wait_nodes:
+        current_node = wait_nodes.popleft()
+        path.append(current_node)
+        for neighbor in g[current_node]:
+            if not visited[neighbor]:
+                wait_nodes.append(neighbor)
+                visited[neighbor] = True
+
+    return path
+
+
+if __name__ == '__main__':
+    graph = nx.Graph()
+    graph.add_nodes_from("ABCDEFGHIJ")
+    graph.add_edges_from([
+        ('A', 'B'),
+        ('A', 'F'),
+        ('B', 'G'),
+        ('F', 'G'),
+        ('G', 'C'),
+        ('G', 'H'),
+        ('G', 'I'),
+        ('C', 'H'),
+        ('I', 'H'),
+        ('H', 'D'),
+        ('H', 'E'),
+        ('H', 'J'),
+        ('E', 'D'),
+    ])
+    # print(graph.nodes)
+    # print(graph['A'])
+    # print(list(graph.neighbors('A')))
+    # drow_graph(graph)
+    print(bfs(graph, 'A'))
