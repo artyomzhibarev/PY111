b169833f11577ab3759a37cfe5a8099186278ed9
Artyom Zhibarev <artyom.zhibarev@yandex.ru>
2021-01-30 15:01:19 +0300

    depth first search implemented

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -1,4 +1,5 @@
 from typing import Hashable, List
+
 import networkx as nx
 
 
@@ -10,5 +11,32 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node of search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    path = []
+    visited = {node: False for node in g.nodes}
+    stack = [start_node]
+    visited[start_node] = True
+    while stack:
+        current_node = stack.pop()
+        path.append(current_node)
+        for node in g.neighbors(current_node):
+            if not visited[node]:
+                stack.append(node)
+                visited[node] = True
+
+    return path
+
+
+if __name__ == '__main__':
+    graph = nx.Graph()
+    graph.add_nodes_from("ABCDEFG")
+    graph.add_edges_from([
+        ('A', 'B'),
+        ('A', 'C'),
+        ('B', 'D'),
+        ('B', 'E'),
+        ('C', 'F'),
+        ('E', 'G'),
+    ])
+    # nx.draw(graph, with_labels=True)
+    # plt.show()
+    print(dfs(graph, 'A'))
