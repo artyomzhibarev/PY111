2962f541b078014f36fbde4dc87b28e49a4a14b6
Artyom Zhibarev <artyom.zhibarev@yandex.ru>
2021-02-07 21:43:30 +0300

    bubble sort implemented

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -1,3 +1,4 @@
+from random import randint
 from typing import List
 
 
@@ -8,4 +9,18 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
+    flag = True
+    iterations = 0
+    while flag:
+        flag = False
+        for i in range(len(container) - iterations - 1):
+            if container[i] > container[i + 1]:
+                container[i], container[i + 1] = container[i + 1], container[i]
+                flag = True
+    iterations += 1
+
     return container
+
+
+if __name__ == '__main__':
+    print(sort([randint(-10, 10) for i in range(10)]))
